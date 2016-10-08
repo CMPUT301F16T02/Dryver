37a432adc461fd981f2fa95a5205eedb92c5c3bf
asanche <asanche@ualberta.ca>
2016-10-08 12:23:15 -0600

    Created some unit tests for Reques, and some shell classes to the tests pass.

---
+++ app/src/androidTest/java/com/example/adam/ubertapp/RequestTests.java
@@ -0,0 +1,50 @@
+package com.example.adam.ubertapp;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Adam on 10/8/2016.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class RequestTests
+{
+    private String default_did = "a5d32d2s_21se2s2";
+    private String default_rid = "b8sjd9sl_28sjd2u";
+    private double default_c = 1.00;
+
+
+    @Test
+    public void testGetCost()
+    {
+        Request r = new Request(10005.40, default_did, default_rid);
+        assertEquals(10005.40, r.getCost(), 0.001);
+    }
+
+    @Test
+    public void testGetaDriver()
+    {
+        String did = "fg2swd";
+
+        Request r = new Request(10005.50, did, default_rid);
+
+        assertEquals("fg2swd", r.getDriverId());
+    }
+
+    @Test
+    public void testGetaRider()
+    {
+        String rid = "fg2swd";
+
+        Request r = new Request(default_c, default_did, rid);
+
+        assertEquals("fg2swd", r.getRiderId());
+    }
+}
---
+++ app/src/main/java/com/example/adam/ubertapp/Request.java
@@ -0,0 +1,34 @@
+package com.example.adam.ubertapp;
+
+/**
+ * Created by Adam on 10/8/2016.
+ */
+public class Request
+{
+    private final double cost;
+    private final String driverId;
+    private final String riderId;
+
+    public Request(double cost, String driverId, String riderId)
+    {
+        this.cost = cost;
+        this.riderId = riderId;
+        this.driverId = driverId;
+    }
+
+
+    public double getCost()
+    {
+        return cost;
+    }
+
+    public String getDriverId()
+    {
+        return driverId;
+    }
+
+    public String getRiderId()
+    {
+        return riderId;
+    }
+}
