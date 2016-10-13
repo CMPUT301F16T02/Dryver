98750f0fa63d6dd3874cc3138fea220845f51609
Andrei <drei3000@gmail.com>
2016-10-13 13:52:06 -0600

    Added ElasticSeach class boiler plate with a few tests.

---
+++ app/src/androidTest/java/com/ubertapp/ElasticSearchTests.java
@@ -0,0 +1,21 @@
+package com.ubertapp;
+
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class ElasticSearchTests {
+
+    // Not sure how we are going to be implementing these ones yet
+    // TODO: Implement other tests for dealing with the server when the class evolves.
+    @Test
+    public void testFetchRequest() {
+        // TODO: test fetching from server
+    }
+
+    @Test
+    public void testPushRequest() {
+        // TODO: test pushing to server
+    }
+}
---
+++ app/src/main/java/com/ubertapp/Controllers/ElasticSearch.java
@@ -0,0 +1,9 @@
+package com.ubertapp.Controllers;
+
+
+/**
+ * The ElasticSearch class that provides helper methods for pushing and pulling from the server.
+ */
+public class ElasticSearch {
+    // TODO: 2016-10-13 Implement different methods for pulling and pushing from the server
+}
