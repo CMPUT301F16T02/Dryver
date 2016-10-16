e603b30f061d957eb9f9e8aec18b8c6977016185
Andrei <drei3000@gmail.com>
2016-10-16 17:17:04 -0600

    fixed adams example ghosts and started an openingpage branch...

--- app/src/main/java/com/ubertapp/Views/ActivityOpeningPage.java
+++ app/src/main/java/com/ubertapp/Views/ActivityOpeningPage.java
@@ -4,12 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import com.ubertapp.R;
 
-public class MainActivity extends Activity {
+public class ActivityOpeningPage extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_opening_page);
     }
 }
