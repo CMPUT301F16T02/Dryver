0a323423717e311b912971addfa2c839abdf787a
Andrei <drei3000@gmail.com>
2016-10-18 17:23:13 -0600

    stringtodate and datetostring conversion methods

--- app/src/main/java/com/ubertapp/Models/HelpMe.java
+++ app/src/main/java/com/ubertapp/Models/HelpMe.java
@@ -1,10 +1,12 @@
 package com.ubertapp.Models;
 
+
 import android.app.Activity;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import com.ubertapp.R;
+import java.util.Date;
+
 
 /**
  * Global helper methods for the ubertapp app. HelpMe stands for Helper Methods.
@@ -29,5 +31,13 @@ public class HelpMe extends Activity {
 //        back = (Button) activity.findViewById(R.id.back_button);
 //        save = (Button) activity.findViewById(R.id.save_button);
         header = (TextView) activity.findViewById(R.id.header_textview);
+    }
+
+    static public void dateToString(Date date) {
+        // TODO: 2016-10-18 implement this
+    }
+
+    static public void stringToDate(String stringDate) {
+        // TODO: 2016-10-18 implement this. 
     }
 }
