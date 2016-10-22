07dbdc28f1b98993cdf72ad05eb64c8d02cd32ff
JiaweiWu <jwu5@ualberta.ca>
2016-10-22 17:13:18 -0600

    Finished UML and added new activities for user profile and map

---
+++ app/src/main/java/com/ubertapp/Activities/ActivityRequestSelection.java
@@ -0,0 +1,16 @@
+package com.ubertapp.Activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.ubertapp.R;
+
+public class ActivityRequestSelection extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_request_selection);
+    }
+
+}
---
+++ app/src/main/java/com/ubertapp/Activities/ActivityUserProfile.java
@@ -0,0 +1,15 @@
+package com.ubertapp.Activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.ubertapp.R;
+public class ActivityUserProfile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_user_profile);
+    }
+
+}
