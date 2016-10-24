6b577fde596ab048137b861a697ba891fdc68cff
asanche <asanche@ualberta.ca>
2016-10-24 14:26:36 -0600

    updated uml with types and missing activities

--- app/src/main/java/com/ubertapp/Controllers/RequestSingleton.java
+++ app/src/main/java/com/ubertapp/Controllers/RequestSingleton.java
@@ -1,15 +1,24 @@
 package com.ubertapp.Controllers;
 
+import com.ubertapp.Models.Request;
+
+import java.util.ArrayList;
+
 /**
  * Created by drei on 2016-10-08.
  */
 public class RequestSingleton {
     private static RequestSingleton ourInstance = new RequestSingleton();
+    private static ArrayList<Request> requests = new ArrayList<Request>();
 
     public static RequestSingleton getInstance() {
         return ourInstance;
     }
 
     private RequestSingleton() {
+    }
+
+    public static ArrayList<Request> getRequests() {
+        return requests;
     }
 }
