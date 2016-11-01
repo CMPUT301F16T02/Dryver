fcb5580feefb43b9e55e2de275f412db6709ea03
Andrei <drei3000@gmail.com>
2016-11-01 16:53:37 -0600

    trying to apply ES to Login

--- app/src/main/java/com/ubertapp/Activities/ActivityLogin.java
+++ app/src/main/java/com/ubertapp/Activities/ActivityLogin.java
@@ -4,10 +4,14 @@ package com.ubertapp.Activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.ubertapp.Controllers.ElasticSearchController;
 import com.ubertapp.Models.HelpMe;
+import com.ubertapp.Models.User;
 import com.ubertapp.R;
 
 
@@ -18,22 +22,33 @@ public class ActivityLogin extends Activity {
 
     private EditText usernameEditText;
     private Button loginButton;
+    private ElasticSearchController ES = new ElasticSearchController();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Need this to run thread-safe networking calls.
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         usernameEditText = (EditText) findViewById(R.id.username_edittext);
 
         loginButton = (Button) findViewById(R.id.login_button);
-
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (!HelpMe.isEmptyTextField(usernameEditText)) {
-                    Intent intent = new Intent(ActivityLogin.this, ActivitySelection.class);
-                    ActivityLogin.this.startActivity(intent);
+                    User user = ES.getUserByID(usernameEditText.getText().toString());
+                    if (user != null) {
+                        // TODO: 2016-11-01 assign the user model the current user.
+
+                        Intent intent = new Intent(ActivityLogin.this, ActivitySelection.class);
+                        ActivityLogin.this.startActivity(intent);
+                    } else {
+                        usernameEditText.setError("Username does not exist.");
+                    }
                 }
             }
         });
