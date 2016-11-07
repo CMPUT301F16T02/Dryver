64d91d71ed9a7fe5e16587c26746278601555ffd
asanche <asanche@ualberta.ca>
2016-11-07 13:33:34 -0700

    have user controller linked to ESController and login activity passing stuff. Not really finalized. The user profile activity is not reachable due to crashes. Stuff is broken yay!

--- app/src/main/java/com/ubertapp/Activities/ActivityLogin.java
+++ app/src/main/java/com/ubertapp/Activities/ActivityLogin.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.ubertapp.Controllers.UserController;
 import com.ubertapp.Models.HelpMe;
 import com.ubertapp.R;
 
@@ -18,6 +20,7 @@ public class ActivityLogin extends Activity {
 
     private EditText usernameEditText;
     private Button loginButton;
+    private UserController userController = UserController.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +35,13 @@ public class ActivityLogin extends Activity {
             @Override
             public void onClick(View view) {
                 if (!HelpMe.isEmptyTextField(usernameEditText)) {
+                    userController.login(usernameEditText.getText().toString());
                     Intent intent = new Intent(ActivityLogin.this, ActivitySelection.class);
                     ActivityLogin.this.startActivity(intent);
                 }
             }
         });
     }
-
-
-
-
-
 
 
     // TODO: 2016-10-16 Generate error handling when database is integrated.
