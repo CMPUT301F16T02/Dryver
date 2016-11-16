14436cebafe17922ea9d8145a8b4e9594283b836
asanche <asanche@ualberta.ca>
2016-11-15 17:07:18 -0700

    added test that uses the UI and the ActivityRegistration to try and register an existing user.

--- app/src/androidTest/java/com/dryver/UITests/ActivityRegistrationTests.java
+++ app/src/androidTest/java/com/dryver/UITests/ActivityRegistrationTests.java
@@ -27,6 +27,7 @@ import com.dryver.Activities.ActivityRegistration;
 import com.dryver.Activities.ActivitySelection;
 import com.dryver.Controllers.ElasticSearchController;
 import com.dryver.Mock.MockElasticSeachController;
+import com.dryver.Models.User;
 import com.dryver.R;
 
 import org.junit.Before;
@@ -42,6 +43,8 @@ import static android.support.test.espresso.intent.matcher.IntentMatchers.hasCom
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * UI Tests for the Registration Activity using Espress
@@ -64,7 +67,7 @@ public class ActivityRegistrationTests {
      */
     @Before
     public void initValidString() {
-        username = "DopeD3aler666";
+        username = "TEST2";
         firstname = "Osama";
         lastname = "Bin Laden";
         phoneNumber = "5555555555";
@@ -102,6 +105,43 @@ public class ActivityRegistrationTests {
         onView(withText("Done")).perform(click());
 
         intended(hasComponent(new ComponentName(getTargetContext(), ActivitySelection.class)));
+    }
+
+    /**
+     * Tests adding tying to add a bad user
+     */
+    @Test
+    public void TestRegisterTakenUser(){
+        ElasticSearchController ES = ElasticSearchController.getInstance();
+        User user = ES.getUser(username);
+        if(user == null)
+        {
+            assertTrue(ES.addUser(new User(username, firstname, lastname, phoneNumber, email)));
+        }
+
+        onView(withText("Registration")).check(ViewAssertions.matches(isDisplayed()));
+
+        onView(withId(R.id.username_edittext)).perform(typeText(username));
+        onView(withText(username)).check(ViewAssertions.matches(isDisplayed()));
+
+        onView(withId(R.id.firstname_edittext)).perform(typeText(firstname));
+        onView(withText(firstname)).check(ViewAssertions.matches(isDisplayed()));
+
+        onView(withId(R.id.lastname_edittext)).perform(typeText(lastname));
+        onView(withText(lastname)).check(ViewAssertions.matches(isDisplayed()));
+
+        onView(withId(R.id.phone_edittext)).perform(typeText(phoneNumber));
+        onView(withText(phoneNumber)).check(ViewAssertions.matches(isDisplayed()));
+
+        onView(withId(R.id.email_edittext)).perform(typeText(email));
+        onView(withText(email)).check(ViewAssertions.matches(isDisplayed()));
+
+        onView(withText("Done")).perform(click());
+
+        onView(withText("Registration")).check(ViewAssertions.matches(isDisplayed()));
+
+        assertTrue(ES.deleteUserByEsID(user));
+        assertNull(ES.getUser(username));
     }
 
 
