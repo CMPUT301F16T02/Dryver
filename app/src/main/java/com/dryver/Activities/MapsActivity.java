ff862b1185b7240ab51a82b1f9af67e211e252cb
jordanvogel <jordan.n.vogel@gmail.com>
2016-11-24 03:42:51 -0700

    Moving branches push

--- app/src/main/java/com/dryver/Activities/MapsActivity.java
+++ app/src/main/java/com/dryver/Activities/MapsActivity.java
@@ -1,36 +1,149 @@
 package com.dryver.Activities;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.dryver.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnMarkerDragListener,
+        GoogleMap.OnMapLongClickListener {
 
-    private GoogleMap mMap;
+    private GoogleMap map;
+    private GoogleApiClient mClient;
+
+    private Location currentLocation;
+    private LocationRequest mLocationRequest;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.hamburgler_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        initializeLocationRequest(100, 1000);
+
+        mClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        mClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        map.clear();
+        LocationServices.FusedLocationApi.requestLocationUpdates(mClient, mLocationRequest, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                currentLocation = location;
+                moveMap(currentLocation);
+            }
+        });
+
+        currentLocation = LocationServices.FusedLocationApi.getLastLocation(mClient);
+        if (currentLocation != null) {
+            moveMap(currentLocation);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        map.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title("Hello world"));
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        map = googleMap;
+        LatLng latLng = new LatLng(53.5, -113.2);
+        map.addMarker(new MarkerOptions().position(latLng).draggable(true));
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        //Setting onMarkerDragListener to track the marker drag
+        map.setOnMarkerDragListener(this);
+        //Adding a long click listener to the map
+        map.setOnMapLongClickListener(this);
     }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
+    }
+
+    public void moveMap(Location location) {
+        LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
+        map.addMarker(new MarkerOptions().position(latlng).draggable(true).title("Current Location"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+        map.animateCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    public void initializeLocationRequest(int LOCATION_UPDATES, int LOCATION_INTERVAL) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setNumUpdates(LOCATION_UPDATES);
+        mLocationRequest.setInterval(LOCATION_INTERVAL);
+    }
+
 }
